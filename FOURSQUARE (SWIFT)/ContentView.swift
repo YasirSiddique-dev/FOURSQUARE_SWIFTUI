9da88bc9d395ea753c538596a857fb84a72333e9
Yasir Siddique <imyasir34@gmail.com>
2021-12-24 23:31:16 +0500

    Initial Commit

---
+++ FOURSQUARE (SWIFT)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FOURSQUARE (SWIFT)
+//
+//  Created by Yasir  on 12/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FOURSQUARE (SWIFT)/FOURSQUARE__SWIFT_App.swift
@@ -0,0 +1,17 @@
+//
+//  FOURSQUARE__SWIFT_App.swift
+//  FOURSQUARE (SWIFT)
+//
+//  Created by Yasir  on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct FOURSQUARE__SWIFT_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
